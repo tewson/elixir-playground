a7df59539a8f5be53e6e9655cd72ec0012c48731
Tewson Seeoun <tewson.seeoun@gmail.com>
2016-10-01 19:12:41 +0100

    Project skeleton from Mix

---
+++ lib/elixir_playground.ex
@@ -0,0 +1,2 @@
+defmodule ElixirPlayground do
+end
---
+++ test/elixir_playground_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirPlaygroundTest do
+  use ExUnit.Case
+  doctest ElixirPlayground
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
